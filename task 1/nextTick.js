a8f35fa81221b8df5e28af321d2bb3bb0244ff3f
Pawel Wasil <wasil.paul@gmail.com>
2018-12-14 00:18:26 +0100

    File size checker added to repo

---
+++ task 1/nextTick.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+function fileSize(filenName, callback) {
+  if (typeof filenName !== "string") {
+    return callback(new TypeError("argument should be a string"));
+  }
+  fs.stat(filenName, (err, stats) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, stats.size);
+  });
+}
+fileSize(__filename, (err, size) => {
+  if (err) throw err;
+  console.log(`Size in KB: ${size / 1024}`);
+});
+
+console.log(`Hello!`);
