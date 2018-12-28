854b1f53f349a947d93af86af42b26c899acc824
Pawel Wasil <wasil.paul@gmail.com>
2018-12-28 22:23:52 +0100

    HTTP agent

--- task4_http/http.js
+++ task4_http/http.js
@@ -1,6 +1,9 @@
+//server: http.Server
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
+  //req: http.IncomingMessage
+  //res: http.ServerResponse
   res.writeHead(200, { "content-type": "text/plain" });
   res.end("Hello from server \n");
 });
--- task4_http/request.js
+++ task4_http/request.js
@@ -1,14 +1,32 @@
 const http = require("http");
 
+/*
 const req = http.request(
   {
     hostname: "www.google.com",
     method: "GET"
   },
   res => {
-    console.log(res);
+    console.log(res.statusCode, res.headers);
+    res.on("data", data => {
+      console.log(data.toString());
+    });
   }
 );
+*/
+
+//req: http.ClientRequest
+const req = http.get("http://www.google.com", res => {
+  //res: http.IncomingMessage
+  console.log(res.headers);
+  res.on("data", data => {
+    console.log(data.toString());
+  });
+});
 
 req.on("error", err => console.log(err));
+console.log(req.agent); //http.Agent
+
+/*
 req.end();
+*/
